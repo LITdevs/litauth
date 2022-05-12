9b05efb8c5cb7bb3f6dd38360377da1ad1593f68
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-05-12 13:03:16 +0300

    h

--- app.js
+++ app.js
@@ -95,30 +95,17 @@ app.use(function (err, req, res, next) {
 app.set('trust proxy', 1);
 function popupMid(req, res, next) {
 	next()
-}/*	if (/MSIE|Trident/.test(req.headers['user-agent'])) return res.render(`${__dirname}/public/error.ejs`, { stacktrace: null, friendlyError: "Your browser is no longer supported by Vukkybox. Please <a href='https://browser-update.org/update-browser.html'>update your browser</a>." });
+	if (/MSIE|Trident/.test(req.headers['user-agent'])) return res.render(`${__dirname}/public/error.ejs`, { stacktrace: null, friendlyError: "Your browser is no longer supported by Vukkybox. Please <a href='https://browser-update.org/update-browser.html'>update your browser</a>." });
 	if (req.headers['user-agent'].indexOf('Safari') != -1 && req.headers['user-agent'].indexOf('Macintosh') == -1 && req.headers['user-agent'].indexOf('OPR') == -1 && req.headers['user-agent'].indexOf('Edge') == -1 && req.headers['user-agent'].indexOf('Chrome') == -1) return res.render(`${__dirname}/public/error.ejs`, { stacktrace: null, friendlyError: "Sorry, but iPhones and iPads are not currently supported by Vukkybox, because Safari is terrible, and all web browsers there are Safari in a trench coat.<br>Please buy a good device, such as an Android phone, or even better... a computer!<br><br><img src='https://dokodemo.neocities.org/images/buttons/phonechump.gif'>" });
 	if (!req.isAuthenticated()) {
 		return next()
 	}
-	let user = req.user._id ? req.user : req.user[0]
-	db.checkPopup(user._id, function (accepted) {
-		if (accepted == 500) return res.send("500: Internal Server Error");
-		if (!accepted) {
-			return res.redirect("/popup")
-		} else {
-			db.checkNews(user._id, accepted => {
-				if (!accepted) req.session.news = true;
-				if (accepted) req.session.news = false;
-				req.session.save();
-				return next()
-			})
-		}
-	})
-}*/
+	next()
+}
 
-app.get('/login', function(req, res) {
+app.get('/', function(req, res) {
 	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
-	res.render(__dirname + '/public/login.ejs', {user: user, gravatarHash: user ? crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex") : null, redirect: req.session.redirectTo != undefined && req.session.redirectTo.length > 1 ? true : false});
+	res.render(__dirname + '/public/index.ejs', {user: user, gravatarHash: user ? crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex") : null, redirect: req.session.redirectTo != undefined && req.session.redirectTo.length > 1 ? true : false});
 });
 
 app.get("/profile", checkAuth, popupMid, function (req, res) {
@@ -149,33 +136,11 @@ app.get('/terms', function(req, res){
 
 app.get('/delete', checkAuth, function(req,res) {
 	user = req.user._id ? req.user : req.user[0]
-	res.render(__dirname + "/public/deleteConfirm.ejs", {csrfToken: req.csrfToken(), twoFactor: user.twoFactor})
+	res.render(__dirname + "/public/deleteConfirm.ejs", {csrfToken: req.csrfToken()})
 })
-/*
-app.post('/delete2fa', checkAuth, function(req, res) {
-	user = req.user._id ? req.user : req.user[0]
-	if(!twoFactor) res.status(400).send("what are you doing.")
-	var verified = speakeasy.totp.verify({ secret: user.twoFactorSecret,
-		encoding: 'base32',
-		token: req.body.otp });
-	if(!verified) {
-		req.session.twoFactorValidated = false;
-		req.session.delete2fa = false; //lets make sure that it is absolutely for sure not allowed to delete without 2fa
-		req.session.save();
-		res.send({verified: false})
-	}
-	if(verified) {
-		req.session.delete2fa = true;
-		req.session.save();
-		res.send({verified: true})
-	}
-	
-	
-})
-*/
+
 app.post("/delete", checkAuth, function(req, res) {
 	user = req.user._id ? req.user : req.user[0]
-	if(user.twoFactor && !req.session.delete2fa) res.redirect("/logout");
 	db.deleteUser(user, function(result) {
 		if(result == 500) {
 			res.redirect('/resources/500.html');
@@ -187,27 +152,18 @@ app.post("/delete", checkAuth, function(req, res) {
 })
 
 app.get('/loginDiscord', passport.authenticate('discord', { scope: scopes, prompt: prompt }), function(req, res) {
-	req.session.twoFactorValidated = false
-	req.session.twoFactorLastValidated = 0
 	req.session.save()
 });
 app.get('/loginGithub', passport.authenticate('github'), function(req, res) {
-	req.session.twoFactorValidated = false
-	req.session.twoFactorLastValidated = 0
 	req.session.save()
 });
 app.get('/loginGoogle', passport.authenticate('google'), function(req, res) {
-	req.session.twoFactorValidated = false
-	req.session.twoFactorLastValidated = 0
 	req.session.save()
 });
 
 app.get('/callbackdiscord',
-	passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) { 
-		req.session.twoFactorValidated = false
-		req.session.twoFactorLastValidated = 0
+	passport.authenticate('discord', { failureRedirect: '/' }), function(req, res) {
 		req.session.save()
-		//if(req.user.twoFactor) return res.redirect('/validate2fa')
 		if(req.session.redirectTo) {
 			let dest = req.session.redirectTo;
 			req.session.redirectTo = "/"
@@ -220,10 +176,7 @@ app.get('/callbackdiscord',
 
 app.get('/callbackgithub',
 	passport.authenticate('github', { failureRedirect: '/' }), function(req, res) { 
-		req.session.twoFactorValidated = false
-		req.session.twoFactorLastValidated = 0
 		req.session.save()
-		//if(req.user.twoFactor) return res.redirect('/validate2fa')
 		if(req.session.redirectTo) {
 			let dest = req.session.redirectTo;
 			req.session.redirectTo = "/"
@@ -235,10 +188,7 @@ app.get('/callbackgithub',
 );
 app.get('/callbackgoogle',
 	passport.authenticate('google', { failureRedirect: '/' }), function(req, res) {
-		req.session.twoFactorValidated = false
-		req.session.twoFactorLastValidated = 0
 		req.session.save()
-		//if(req.user.twoFactor) return res.redirect('/validate2fa')
 		if(req.session.redirectTo) {
 			let dest = req.session.redirectTo;
 			req.session.redirectTo = "/"
@@ -249,43 +199,14 @@ app.get('/callbackgoogle',
 	} // auth success
 );
 
-/*
-app.get('/otpcallback', function(req, res) {
-	if(!req.isAuthenticated()) return res.redirect('/login')
-	if(!req.user.twoFactor) return res.redirect('/2fa')
-	if(!req.session.twoFactorValidated) return res.redirect('/validate2fa')
-	if(req.session.redirectTo) {
-		let dest = req.session.redirectTo;
-		req.session.redirectTo = "/"
-		res.redirect(dest) 
-	} else {
-		res.redirect('/')
-	}
-})
-*/
-
 app.get('/logout', function(req, res) {
 	req.logout();
 	res.redirect('/');
 });
-/*
-app.get("/popup", checkAuth, function (req, res) {
-	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
-	res.render(__dirname + '/public/popup.ejs', {csrfToken: req.csrfToken(), user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex"), redirect: req.session.redirectTo != undefined && req.session.redirectTo.length > 1 ? true : false});
-	
-})
-
-app.post('/popup', checkAuth, function (req, res) {
-	if(req.body.popup != "yes") return res.redirect("/delete")
-	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
-	db.acceptPopup(user._id)
-	res.redirect("/")
-})*/
 
 function checkAuth(req, res, next) {
 	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
 	if(user) {
-		//if (user.twoFactor && !req.session.twoFactorValidated) return res.redirect("/validate2fa")
 		db.lastLogin(user, function(newBalance, newUser) {
 			req.session.passport.user = newUser
 			req.session.passport.user.balance = newBalance
@@ -294,130 +215,13 @@ function checkAuth(req, res, next) {
 		return next();
 	}
 	req.session.redirectTo = req.path;
-	res.redirect(`/login`)
+	res.redirect(`/`)
 }
 
-/*function checkAuthnofa(req, res, next) {
-	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
-	if(user) {
-		db.lastLogin(user, function(newBalance, newUser) {
-			req.session.passport.user = newUser
-			req.session.passport.user.balance = newBalance
-			req.session.save()
-		})
-		return next();
-	}
-	req.session.redirectTo = req.path;
-	res.redirect(`/login`)
-}
-
-function checkAuthtime(req, res, next) {
-	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
-	if(!user) return res.redirect("/login")
-	if(user) {
-		db.lastLogin(user, function(newBalance, newUser) {
-			req.session.passport.user = newUser
-			req.session.passport.user.balance = newBalance
-			req.session.save()
-		})
-		if(!user.twoFactor) return next();
-		if(!req.session.twoFactorValidated) return res.redirect("/validate2fa")
-		let diffMs = Date.now() - req.session.twoFactorLastValidated;
-
-		if (diffMs > 1800000) return res.redirect("/validate2fa")
-		return next();
-	}
-	res.send("what")
-}
-app.get('/2fa', checkAuthnofa, function(req, res) {
-	let user = req.user._id ? req.user : req.user[0];
-	db.getUser(user._id, user => {
-		if(user.twoFactor) return res.render(`${__dirname}/public/2fareset.ejs`, {csrfToken: req.csrfToken(), user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
-		let secret = speakeasy.generateSecret({name: "Vukkybox 2FA"});
-		req.session.two_factor_temp_secret = secret.base32;
-		req.session.save()
-		qrcode.toDataURL(secret.otpauth_url, function(err, dataUrl) {
-			if (err) return res.render(__dirname + '/public/error.ejs', {stacktrace: null, friendlyError: "Something went wrong while starting the 2FA flow. <br>For your privacy the stacktrace is hidden, if this happens again please contact us."});
-			res.render(`${__dirname}/public/2fa.ejs`, {csrfToken: req.csrfToken(), user: user, qrDataUrl: dataUrl, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
-		});
-	})
-});
-
-app.get('/validate2fa', function(req, res) {
-	if (!req.isAuthenticated) return res.redirect("/login");
-	let user = req.user._id ? req.user : req.user[0];
-	db.getUser(user._id, user => {
-		if(!user.twoFactor) return res.send("you dont even have 2FA enabled lol");
-		res.render(`${__dirname}/public/validate2fa.ejs`, {csrfToken: req.csrfToken(), user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});	
-	})
-});
-
-app.post('/votp', checkAuthnofa, function(req, res) {
-	let user = req.user._id ? req.user : req.user[0];
-	db.getUser(user._id, user => {
-		var verified = speakeasy.totp.verify({ secret: user.twoFactorSecret,
-			encoding: 'base32',
-			token: req.body.otp });
-		if(!verified) {
-			req.logout()
-			return res.send({valid: false});
-		}
-		if(verified) {
-			res.send({valid: true});
-			req.session.twoFactorValidated = true;
-			req.session.twoFactorLastValidated = Date.now();
-			req.session.save();
-		}
-	})
-})
-
-app.post('/fotp', checkAuth, function(req, res) {
-	let user = req.user._id ? req.user : req.user[0];
-	if(user.twoFactor) return res.status(403).send("2fa already enabled");
-	if(!req.session.two_factor_temp_secret) return res.status(400).send("2fa flow not started");
-	let userInput = req.body.otp;
-	var verified = speakeasy.totp.verify({ secret: req.session.two_factor_temp_secret,
-		encoding: 'base32',
-		token: userInput });
-	if(!verified) return res.status(400).send({valid: false});
-	db.enabletwoFactor(user._id, req.session.two_factor_temp_secret);
-	res.send({valid: true});
-})
-
-
-app.post('/emailCode', checkAuthnofa, function(req, res) {
-	let user = req.user._id ? req.user : req.user[0];
-	let secret = speakeasy.generateSecret({length: 8});
-	user.emailCode = secret.base32;
-	req.session.emailCode = secret.base32;
-	req.session.save()
-	db.sendEmail(user, fs.readFileSync(`${__dirname}/public/email/emailCode.html`, "utf8"), "Vukkybox Authenticator recovery code");
-})
-
-app.post('/emailCheckCode', checkAuthnofa, function(req, res) {
-	let user = req.user._id ? req.user : req.user[0];
-	if(!req.session.emailCode) return res.status(400).send({valid: false});
-	if(req.body.otp != req.session.emailCode) return res.status(400).send({valid: false});
-	db.disabletwoFactor(user._id);
-	res.send({valid: true});
-})
-
-app.post('/2fareset', checkAuthnofa, function(req, res) {
-	let user = req.user._id ? req.user : req.user[0];
-	db.getUser(user._id, user => {
-		var verified = speakeasy.totp.verify({ secret: user.twoFactorSecret,
-			encoding: 'base32',
-			token: req.body.otp });
-		if(!verified) return res.status(400).render(`${__dirname}/public/2fareset.ejs`, {failure: true, csrfToken: req.csrfToken(), user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
-		if(verified) db.disabletwoFactor(user._id);
-		if(verified) res.render(`${__dirname}/public/2fareset.ejs`, {successful: true, csrfToken: req.csrfToken(), user: user, gravatarHash: crypto.createHash("md5").update(user.primaryEmail.toLowerCase()).digest("hex")});
-	})
-})
-*/
 app.use(function (err, req, res, next) {
 	console.error(err.stack);
 	if(err.message == 'Invalid "code" in request.') {
-		return res.status(500).render(`${__dirname}/public/error.ejs`, { stacktrace: null, friendlyError: "It looks like we couldn't log you in. Would you mind <a href='/login'>trying that again</a>?" });
+		return res.status(500).render(`${__dirname}/public/error.ejs`, { stacktrace: null, friendlyError: "It looks like we couldn't log you in. Would you mind <a href='/'>trying that again</a>?" });
 	}
 	res.status(500).render(`${__dirname}/public/error.ejs`, { stacktrace: err.stack, friendlyError: null });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
 	"./public/common/*.ejs",
   "./public/resources/*.js"
   ],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      vukky: "#00a8f3",
+      vwhite: "#f8f7ff",
+      vgray: "#494949"
+    }
   },
   variants: {
     extend: {}
