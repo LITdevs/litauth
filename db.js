0b4b196a164190dc00f088b1087c646e15668fce
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-05-26 11:43:09 +0300

    intendation to tabs, database functionality for registration and login

--- db.js
+++ db.js
@@ -8,22 +8,22 @@ db.on('error', console.error.bind(console, 'connection error:'));
 var User
 var Code
 db.once('open', function() {
-  const userSchema = new mongoose.Schema({
+	const userSchema = new mongoose.Schema({
 	username: String,
 	passwordHash: Buffer,
 	salt: Buffer
-  });
-  User = mongoose.model('User', userSchema);
+	});
+	User = mongoose.model('User', userSchema);
 });
 /*let transporter = nodemailer.createTransport({
-    host: "smtp.zoho.eu",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "vukkybox@litdevs.org",
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+		host: "smtp.zoho.eu",
+		port: 465,
+		secure: true,
+		auth: {
+			user: "vukkybox@litdevs.org",
+			pass: process.env.EMAIL_PASS,
+		},
+	});
 
 async function sendEmail(user, emailContent, emailSubject) {
 	let parsedEmailContent = emailContent.replaceAll("$username", user.username)
@@ -33,9 +33,46 @@ async function sendEmail(user, emailContent, emailSubject) {
 		to: user.primaryEmail,
 		subject: emailSubject,
 		html: parsedEmailContent
-	  });
+		});
 }
 */
 
+function login(email, callback) {
+	User.findOne({email: email.toLowerCase()}, function (err, user) {
+			if (err) return (callback(err, null));
+			callback(null, user);
+	});
+}
+
+function createAccount(email, username, password, salt, cb) {
+	let user = new User({
+				email: email,
+				username:username,
+				passwordHash:password,
+				salt:salt
+	})
+	user.save(function (err, user) {
+		if (err) {
+			cb({error: err, success: null})
+			return console.error(err);
+		}
+		return cb({error: null, success: true})
+	});
+}
+
+function checkEmail(email, cb) {
+	User.findOne({email:email}, (err, res) => {
+		if (err) {
+			cb(true)
+			return console.error(err);
+		}
+		if(res) return cb("used")
+		cb(null)
+	})
+}
+
 module.exports = {
+	login,
+	checkEmail,
+	createAccount
 }
