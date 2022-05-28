fc49f99096a9857cbe3eca23591aaabc60b1ceab
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-05-28 13:51:14 +0300

    :apple:

---
+++ emailTest.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const fs = require("fs");
+let transporter = nodemailer.createTransport({
+    host: "mail.wanderers.cloud",
+    port: 465,
+    secure: false,
+    auth: {
+        user: "litauth",
+        pass: process.env.EMAIL_PASS
+    },
+});
+
+transporter.sendMail({
+    from: '"LITauth" <litauth@litdevs.org>',
+    bcc: "contact@litdevs.org",
+    subject: "LITauth Account Verification",
+    html: fs.readFileSync("./verificationTemplate.html", "utf8").toString()
+});
