c1ac3e0f25caa1e854cbf622e255fb09462736fd
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-11-14 09:54:24 +0100

    your code is now my property

--- app.js
+++ app.js
@@ -342,8 +342,12 @@ app.post('/login/password', passport.authenticate('local', {
 	failureRedirect: '/',
 	failureFlash: true
 }), (req, res) => {
-	console.log(req.session.redirectTo)
-	res.redirect(req.session.redirectTo ? req.session.redirectTo : '/profile');
+	if (req.cookies['redirectTo']) {
+		res.clearCookie("redirectTo");
+		res.redirect(req.cookies['redirectTo']);
+	} else {
+		res.redirect('/profile');
+	}
 });
 
 app.get('/register', (req, res) => {
@@ -359,8 +363,7 @@ function checkAuth(req, res, next) {
 	let user = req.isAuthenticated() ? req.user._id ? req.user : req.user[0] : null
 	if(user) return next();
 	if(req.method == 'POST') return res.status(403).send('You are not logged in.');
-	req.session.redirectTo = req.url;
-	req.session.save();
+	res.cookie('redirectTo', req.url);
 	res.redirect(`/`)
 }
 
